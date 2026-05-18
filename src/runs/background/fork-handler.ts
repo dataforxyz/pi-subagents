dataforxyz/agent-intercom-pi

Clarify handler notification mode in prompts

fork-handler.ts
@@ -49,6 +49,8 @@ export interface IntercomForkHandlerRun {
   stdoutPath: string;
   stderrPath: string;
   sessionDir: string;
+  notify?: InboundForkNotify;
+  triggerParentOnSummary?: boolean;
   startedAt: number;
   endedAt?: number;
   exitCode?: number | null;
@@ -161,6 +163,22 @@ function buildEventPayload(entry: InboundForkMessageEntry, run: IntercomForkHand
   };
 }
 
+function parentNotificationModeLines(run: IntercomForkHandlerRun): string[] {
+  const notify = run.notify ?? "summary";
+  if (notify === "none") {
+    return [
+      "Parent notification mode: none",
+      "Your final response is stored in handler logs only and will not be automatically posted to the parent transcript/context.",
+    ];
+  }
+  return [
+    `Parent notification mode: ${notify}`,
+    `Your final response WILL be copied into the parent transcript/context${run.triggerParentOnSummary ? " and will trigger a parent turn" : ""}.`,
+    ...(notify === "ack-and-summary" ? ["The parent already received a launch ack; do not repeat startup details unless relevant."] : []),
+    "Keep the final response concise. If you already sent an intercom message to the parent, do not repeat its full content; just note that you escalated it.",
+  ];
+}
+
 export function buildIntercomForkHandlerPrompt(entry: InboundForkMessageEntry, run: IntercomForkHandlerRun, eventJson: string): string {
   const senderTarget = entry.from.name || entry.from.id;
   const replyGuidance = entry.message.expectsReply
@@ -183,6 +201,7 @@ export function buildIntercomForkHandlerPrompt(entry: InboundForkMessageEntry, r
     "- Escalate only for destructive actions, ambiguous user preference, external side effects, security/privacy/cost risk, conflict with parent work, or low confidence.",
     "- Use intercom.send for non-blocking notices. Use intercom.ask only for true blocking parent decisions.",
     "- Keep any parent escalation brief and include the handler id.",
+    ...parentNotificationModeLines(run).map((line) => `- ${line}`),
     "",
     ...replyGuidance,
     "",
@@ -206,6 +225,7 @@ export function buildIntercomForkHandlerSystemPrompt(run: IntercomForkHandlerRun
     "You have delegated authority to answer or act when safe and derivable.",
     "Escalate only for destructive actions, ambiguous user preference, external side effects, security/privacy/cost risk, conflict with parent work, or low confidence.",
     "For intercom asks, answer directly with intercom.send + replyTo when safe; otherwise escalate quickly.",
+    ...parentNotificationModeLines(run),
     `Handler id: ${run.id}`,
   ].join("\n");
 }
@@ -311,6 +331,8 @@ export async function launchIntercomForkHandler(pi: ExtensionAPI, ctx: Extension
     stdoutPath: path.join(dir, "stdout.log"),
     stderrPath: path.join(dir, "stderr.log"),
     sessionDir: path.join(dir, "sessions"),
+    notify: config.notify,
+    triggerParentOnSummary: config.triggerParentOnSummary,
     startedAt: Date.now(),
   };
   const eventJson = JSON.stringify(buildEventPayload(entry, run, ctx, pi), null, 2);

dataforxyz/pi-subagents

src/runs/background/fork-handler.ts
@@ -43,6 +43,8 @@ interface BackgroundForkRun {
 	exitCode?: number | null;
 	signal?: NodeJS.Signals | null;
 	error?: string;
+	notify: BackgroundForkHandlerNotify;
+	triggerParentOnSummary: boolean;
 	pid?: number;
 }
 
@@ -129,6 +131,21 @@ export function resolveBackgroundForkHandlersConfig(config?: BackgroundForkHandl
 	};
 }
 
+function parentNotificationModeLines(run: BackgroundForkRun): string[] {
+	if (run.notify === "none") {
+		return [
+			"Parent notification mode: none",
+			"Your final response is stored in handler logs only and will not be automatically posted to the parent transcript/context.",
+		];
+	}
+	return [
+		`Parent notification mode: ${run.notify}`,
+		`Your final response WILL be copied into the parent transcript/context${run.triggerParentOnSummary ? " and will trigger a parent turn" : ""}.`,
+		...(run.notify === "ack-and-summary" ? ["The parent already received a launch ack; do not repeat startup details unless relevant."] : []),
+		"Keep the final response concise. If you already sent an intercom message to the parent, do not repeat its full content; just note that you escalated it.",
+	];
+}
+
 function buildSystemPrompt(run: BackgroundForkRun): string {
 	return [
 		"You are a background pi-subagents event handler in a sibling Pi process.",
@@ -140,6 +157,7 @@ function buildSystemPrompt(run: BackgroundForkRun): string {
 		"Use intercom({ action: \"send\", to: <parent>, message: ... }) for required actionable non-blocking parent notices; use intercom.ask only for true blocking decisions.",
 		"Escalate only for destructive actions, ambiguous user preference, external side effects, security/privacy/cost risk, conflict with current parent work, or low confidence.",
 		...(run.parentIntercomTarget ? [`Parent intercom target: ${run.parentIntercomTarget}`] : []),
+		...parentNotificationModeLines(run),
 		`Handler id: ${run.id}`,
 	].join("\n");
 }
@@ -163,6 +181,8 @@ function buildPrompt(event: SubagentBackgroundForkEvent, run: BackgroundForkRun)
 		"```",
 		"",
 		"## Instructions",
+		"",
+		...parentNotificationModeLines(run),
 		"",
 		"Handle this background event without waking the parent feed for routine summaries. If the event includes a child session or artifact path, read it only when it helps triage accurately.",
 		"Do safe checks in this fork. If your conclusion is routine success, optional follow-up, or no action needed, do not send an intercom message to the parent; just state that in your final summary.",
@@ -236,6 +256,8 @@ export async function deliverBackgroundForkEvent(
 				startedAt: Date.now(),
 				...(event.parentSessionFile ? { parentSessionFile: event.parentSessionFile } : {}),
 				...(event.parentIntercomTarget ? { parentIntercomTarget: event.parentIntercomTarget } : {}),
+				notify: resolved.notify,
+				triggerParentOnSummary: resolved.triggerParentOnSummary,
 			};
 		})();
 